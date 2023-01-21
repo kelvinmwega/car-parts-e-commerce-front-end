b66de239d97d6fbd88201dacc16973564ae6a649
kelvin.mwega <kmwega@thrivy.co.ke>
2023-01-21 23:47:04 +0300

    first commit

---
+++ src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signin = () => (
+  <Layout title="Sign In" description="Node React E-commerce App">
+    ...
+  </Layout>
+);
+
+export default Signin;
